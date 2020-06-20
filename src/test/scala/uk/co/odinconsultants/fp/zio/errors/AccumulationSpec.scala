51f88e2e8ead1729684115e66d064854e5c2931e
Phillip Henry <PhillHenry@gmail.com>
2020-06-20 12:46:23 +0100

    Error accumulation.

--- src/test/scala/uk/co/odinconsultants/fp/zio/errors/AccumulationSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/zio/errors/AccumulationSpec.scala
@@ -1,20 +1,23 @@
 package uk.co.odinconsultants.fp.zio.errors
 
-import zio.blocking.Blocking
-import zio.clock.Clock
-import zio.duration._
-import zio.stream.ZStream
+import zio.ZIO.fail
 import zio.test.Assertion._
-import zio.test.TestAspect._
 import zio.test.environment.TestEnvironment
 import zio.test.{DefaultRunnableSpec, ZSpec, _}
-import zio.ZIO
+import zio.{IO, ZIO}
+import zio.test.TestAspect._
 
 object AccumulationSpec extends DefaultRunnableSpec {
 
-  override def spec: ZSpec[TestEnvironment, Any] = suite("Errors should be accumulated")(testM("from ZIOs") {
-    val result =ZIO.fail(1) *> ZIO.fail(2)
-    assertM(result.either)(equalTo(Left(1)))
-  })
+  val failure1: IO[Int, Nothing] = fail(1)
+  val failure2: IO[Int, Nothing] = fail(2)
+
+  override def spec: ZSpec[TestEnvironment, Any] = suite("Errors should be accumulated")(
+    testM("from ZIOs") {
+      val failures  = List(failure1, failure2)
+      val result: ZIO[Any, Int, List[Nothing]] = ZIO.collectAllPar(failures)
+      assertM(result.either)(equalTo(Left(1)))
+    } @@ ignore
+  )
 
 }
