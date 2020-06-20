d5c6e97a7d543431027fa234fec7865153885fe6
Phillip Henry <PhillHenry@gmail.com>
2020-06-20 12:50:14 +0100

    collectAll is non-deterministic. Ignored. validate fails on first failure.

--- src/test/scala/uk/co/odinconsultants/fp/zio/errors/AccumulationSpec.scala
+++ src/test/scala/uk/co/odinconsultants/fp/zio/errors/AccumulationSpec.scala
@@ -12,12 +12,17 @@ object AccumulationSpec extends DefaultRunnableSpec {
   val failure1: IO[Int, Nothing] = fail(1)
   val failure2: IO[Int, Nothing] = fail(2)
 
+  val failures  = List(failure1, failure2)
+
   override def spec: ZSpec[TestEnvironment, Any] = suite("Errors should be accumulated")(
-    testM("from ZIOs") {
-      val failures  = List(failure1, failure2)
+  testM("from ZIOs") {
       val result: ZIO[Any, Int, List[Nothing]] = ZIO.collectAllPar(failures)
       assertM(result.either)(equalTo(Left(1)))
-    } @@ ignore
+    } @@ ignore,
+    testM("from ZIOs") {
+      val result: ZIO[Any, Int, (Nothing, Nothing)] = failure1.validate(failure2)
+      assertM(result.either)(equalTo(Left(1)))
+    }
   )
 
 }
