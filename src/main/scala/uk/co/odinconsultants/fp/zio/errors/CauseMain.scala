5ba302a6fe8f75c7d9942bb760eac65f17ae52e4
Phillip Henry <PhillHenry@gmail.com>
2020-07-06 13:52:32 +0100

    Refactored.

--- src/main/scala/uk/co/odinconsultants/fp/zio/errors/CauseMain.scala
+++ src/main/scala/uk/co/odinconsultants/fp/zio/errors/CauseMain.scala
@@ -1,22 +1,22 @@
 package uk.co.odinconsultants.fp.zio.errors
 
-import java.io.{IOException, InputStream, PipedInputStream, PipedOutputStream}
-
-import zio.blocking.{Blocking, effectBlocking}
-import zio.clock.Clock
-import zio.stream.ZStream
-import zio.{Chunk, ExitCode, IO, UIO, URIO, ZEnv, ZIO}
+import zio._
 
 object CauseMain extends zio.App {
 
-  val someList = List(1, 2, 3)
-
+  val someList = List(1, 2, 3, 4)
 
   def run(args: List[String]): URIO[ZEnv, ExitCode] = {
-    val x = ZIO.foreachParN(someList.size)(someList)(item => IO(println(item)).cause) // ZIO[Any, Nothing, List[Cause[Throwable]]]
-      .map(_.reduce(_ && _)) // ZIO[Any, Nothing, Cause[Throwable]]
-      .uncause
-    x.catchAll(e => UIO(e.printStackTrace())).as(ExitCode.success)
+    val allResults:       ZIO[Any, Nothing,   List[Cause[Throwable]]] = ZIO.foreachParN(someList.size)(someList)(item => toEffect(item).cause)
+    val aggregatedErrors: ZIO[Any, Nothing,   Cause[Throwable]]       = allResults.map(_.reduce(_ && _))
+    val uncaused:         ZIO[Any, Throwable, Unit]                   = aggregatedErrors.uncause
+    val purged:           ZIO[Any, Nothing,   Unit]                   = uncaused.catchAll(e => UIO(e.printStackTrace()))
+    purged.as(ExitCode.success)
   }
 
+  private def toEffect(x: Int): IO[Throwable, Unit] = if (x % 2 == 0) {
+    IO(println(x))
+  } else {
+    IO(throw new Exception(s"$x"))
+  }
 }
